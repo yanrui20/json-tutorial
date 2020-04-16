77086bd4a167fdb52bd78ecf745655103a0aca0a
yanrui20 <1657066071@qq.com>
2020-04-16 23:24:06 +0800

    完成章节1练习 -- 修改

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -21,10 +21,10 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
     v->type = LEPT_NULL;
-    lept_parse_whitespace(c);
+    /*lept_parse_whitespace(c);
     if(c->json[0] != '\0') {
         return LEPT_PARSE_ROOT_NOT_SINGULAR;
-    }
+    }*/
     return LEPT_PARSE_OK;
 }
 
@@ -34,11 +34,11 @@ static int lept_parse_true(lept_context* c, lept_value* v) {
         return LEPT_PARSE_INVALID_VALUE;
     }
     c->json += 3;
-    lept_parse_whitespace(c);
+    /*lept_parse_whitespace(c);
     if(c->json[0] != '\0') {
         v->type = LEPT_NULL;
         return LEPT_PARSE_ROOT_NOT_SINGULAR;
-    }
+    }*/
     v->type = LEPT_TRUE;
     return LEPT_PARSE_OK;
 }
@@ -49,11 +49,11 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
         return LEPT_PARSE_INVALID_VALUE;
     }
     c->json += 4;
-    lept_parse_whitespace(c);
+    /*lept_parse_whitespace(c);
     if(c->json[0] != '\0') {
         v->type = LEPT_NULL;
         return LEPT_PARSE_ROOT_NOT_SINGULAR;
-    }
+    }*/
     v->type = LEPT_FALSE;
     return LEPT_PARSE_OK;
 }
@@ -70,11 +70,18 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int ret;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if(c.json[0] != '\0') {
+            return LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
+    }
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
